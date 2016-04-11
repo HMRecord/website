e925c89dfbbfa412a5e9e63b5ee9653949151d41
joshuagruenstein <joshuagruenstein@gmail.com>
2016-04-11 19:09:43 -0400

    add author to backend

--- js/backend.js
+++ js/backend.js
@@ -1,17 +1,33 @@
 var getArticle = {
 	byID: function(id) {
-		var article = {id:555,
-					   title:"The Spirit of Triumph",
-				       author:{id:69,name:"Donald Trump"},
-				   	   date:"May 7, 2018",
-				   	   content:"Hello World."};
+		var article = {
+			id:555,
+			title:"The Spirit of Triumph",
+			author: {
+				id:69,
+				name:"Donald Trump"
+			},
+			date:"May 7, 2018",
+			content:"Hello World."
+		};
 
 		return article;
 	},
 	byQuery: function(query) {
 		return ["hello","world"];
 	},
-	byAuthor: function(author) {
+	byAuthor: function(authorID) {
 		return ["hello","world"];
+	}
+}
+
+var getAuthor = {
+	byID: function(id) {
+		var author = {
+			id:69,
+			name:"Donald Trump"
+		};
+
+		return author;
 	}
 }
