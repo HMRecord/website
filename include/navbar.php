138d782d8c11fee6316735aa3f009dfbfdb355ba
joshuagruenstein <joshuagruenstein@gmail.com>
2016-04-11 12:50:51 -0400

    organize html

---
+++ include/navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default navbar-static-top">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+		<form class="navbar-form navbar-left" role="search">
+			<div class="form-group">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Search">
+					<span class="input-group-btn">
+					<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+					</span>
+				</div>
+			</div>
+		</form>
+		<ul class="nav navbar-nav navbar-right">
+			<li class="dropdown" style="margin-right:20px">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sections<span class="caret"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Opinions</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Lions Den</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="shortcut icon" href="img/favicon.ico" />
+		<title>HM Record</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/index.css" rel="stylesheet">
+	</head>
+	<body>
+		<?php include 'include/navbar.php'; ?>
+
+		<div class="container">
+		</div>
+
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="js/index.js"></script>
+	</body>
+</html>
