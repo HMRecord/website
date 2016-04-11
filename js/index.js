018b30b493996928fd19d8c65cb1135a356249fe
joshuagruenstein <joshuagruenstein@gmail.com>
2016-04-11 17:43:45 -0400

    set date

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
+function setDate() {
+	var monthNames = [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December"
+	];
+
+	var date = new Date();
+	var day = date.getDate();
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	$("#headerDate").text(monthNames[monthIndex] + " " + day + ", " + year);
+}
+
 $(document).ready(function() {
+	setDate();
+
 	$("#searchBtn").click(function() {
 		var query = $("#searchField").val();
 		if (query !== "") {
