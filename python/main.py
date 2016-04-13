adcbadd5ac858780934af293ea3594bf240eb176
Michael Truell <truell20@gmail.com>
2016-04-13 10:12:06 -0400

    File uploading

--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask.ext.cors import CORS
+from werkzeug import secure_filename
 from pymongo import MongoClient
 from bson.json_util import dumps
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "storage"
 CORS(app)
 
 client = MongoClient()
@@ -46,5 +49,19 @@ def section():
         if 'title' in params:
             db.team.insert_one(params)
 
+
+@app.route('/api/files', methods=['POST'])
+def file():
+    def allowed_file(filename):
+        return '.' in filename and \
+           filename.rsplit('.', 1)[1] in set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
