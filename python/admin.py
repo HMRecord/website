884f6c4fb451a7a276cd06bd9f6e8223c654413d
Michael Truell <truell20@gmail.com>
2016-04-14 12:26:54 -0400

    Admin api authentication

---
+++ python/admin.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, make_response
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__, static_url_path="")
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'admin':
+        return 'sdfkasldfj'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
