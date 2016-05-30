3cb070c3271c136dfdab11201bd563c5f72ee665
Michael Truell <michael_truell@horacemann.org>
2016-05-30 08:30:53 -0400

    Add mac support in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 # Downloads
-sudo apt-get install python3 python3-pip mongodb
+if [ "$(uname)" == "Darwin" ]; then
+    brew install python3 mongodb
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt-get install python3 python3-pip mongodb
+fi
+
 sudo pip3 install flask bson pymongo flask-cors flask-httpauth
 
 # File structure
 mkdir storage
 sudo chmod 755 storage
+
+# Mongo DB required folder
+sudo mkdir -p /data/db
