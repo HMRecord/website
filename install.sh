9008ae05a0bad86c653e4ae69f684248c7664a23
Michael Truell <michael_truell@horacemann.org>
2016-05-30 09:47:07 -0400

    son over apt-get

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     brew install python3 mongodb
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo apt-get install python3 python3-pip mongodb
+    sudo apt-get install python3 python3-pip python-bson mongodb
 fi
 
 sudo pip3 install flask bson pymongo flask-cors flask-httpauth || sudo pip install flask bson pymongo flask-cors flask-httpauth
