60579e8eab331849b4490c6e991d3f5990488899
Henry Wildermuth <FlyingGraysons@users.noreply.github.com>
2016-09-20 22:17:49 -0400

    Update install.sh with gunicorn

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Downloads
 brew install python3 mongodb || sudo apt-get install python3 python3-pip mongodb
-sudo pip3 install flask pymongo flask-cors flask-httpauth || sudo pip install flask pymongo flask-cors flask-httpauth
+sudo pip3 install flask pymongo flask-cors flask-httpauth gunicorn || sudo pip install flask pymongo flask-cors flask-httpauth gunicorn
 
 # File structure
 mkdir storage
