281c9fab1f5247ae882e6dd86435e676496de3a8
joshuagruenstein <joshuagruenstein@gmail.com>
2016-11-28 16:53:10 -0500

    add template about page

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>HM Record: About</title>
+
+		<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic|Lato:400,300,700,400italic,300italic' rel='stylesheet' type='text/css'>
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="css/general.css" rel="stylesheet">
+		<!--<link href="css/about.css" rel="stylesheet">-->
+	</head>
+	<body>
+		<?php include 'include/navbar.php'; ?>
+
+		<div class="container">
+			<div class="row">
+                <div class="col-md-12">
+                    <h1>About the Record</h1>
+                    <p>Blah blah blah blah.  Blah blah blah blah. You can contact us at <a href="mailto:record@horacemann.org">record@horacemann.org</a></p>
+                    <hr>
+                </div>
+			</div>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+
+		<script src="js/general.js"></script>
+		<script src="js/backend.js"></script>
+		<script src="js/index.js"></script>
+	</body>
+</html>
